cb9dec9994949f37bb8c1010aff6541916ff4c68
lain62 <laimadelain@gmail.com>
2024-08-06 11:35:06 +0700

    ADDED THE ABILITY TO INCREASE AND DECREASE TEXT

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::sync::Mutex;
-use tauri::State;
+use tauri::{Manager, State};
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    message: String,
+}
 
 struct Font {
     size: Mutex<i32>,
@@ -22,12 +27,36 @@ fn add_font_size(state: State<Font>) -> i32 {
     *counter
 }
 
+#[tauri::command]
+fn decrease_font_size(state: State<Font>) -> i32 {
+    let mut counter = state.size.lock().unwrap();
+    *counter = *counter - 1;
+
+    *counter
+}
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let id = app.listen_global("font_size_change", |_| {});
+            app.unlisten(id);
+            app.emit_all(
+                "font_size_change",
+                Payload {
+                    message: "Font size changed!".into(),
+                },
+            )
+            .unwrap();
+            Ok(())
+        })
         .manage(Font {
             size: Mutex::new(16),
         })
-        .invoke_handler(tauri::generate_handler![get_font_size, add_font_size])
+        .invoke_handler(tauri::generate_handler![
+            get_font_size,
+            add_font_size,
+            decrease_font_size
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Maintext.tsx
+++ src/components/Maintext.tsx
@@ -1,14 +1,20 @@
 import { invoke } from "@tauri-apps/api"
+import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
 
 function Maintext() {
-    const [fontSize, setFontSize] = useState<any>(0)
-    const getFontSize = async () => await invoke('get_font_size')
-    
+    const [fontSize, setFontSize] = useState<any>(0);
+    const getFontSize = async () => await invoke('get_font_size');
+    const unlisten = async () => await listen('font_size_change', () => {
+        setChangeFontSize(true)
+    });
+    const [changeFontSize, setChangeFontSize] = useState(false)
+
     useEffect(() => {
-        getFontSize().then(val => { setFontSize(val) });
-        console.log(getFontSize())
-    })
+        getFontSize().then(val => { setFontSize(val); console.log(val) });
+        unlisten();
+        setChangeFontSize(false)
+    }, [changeFontSize])
 
     return (
         <textarea style={{ fontSize: fontSize }} className=" p-2 w-3/4 h-[calc(100vh-2.5rem)] resize-none focus:border-none focus:outline-none bg-gray-900 text-white" />
